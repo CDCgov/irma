3ded40d1c124a6b40bd8dcd564bd7500de9cdf69
vfn4 <vfn4@cdc.gov>
2016-03-04 12:38:03 -0500

    Fixed ASSEM_REF sorting and reporting.

--- IRMA_RES/modules/FLU/config/FLU-ref.sh
+++ IRMA_RES/modules/FLU/config/FLU-ref.sh
@@ -5,7 +5,17 @@ PARAM_FILE_VERSION="1.0"
 PARAM_FILE_DATE="2015-12-21"
 
 # Alternative reference
-CUSTOM_REF_FILE="example.fa"	# custom ref file
+CUSTOM_REF_FILE="example.fasta"	# custom ref file
+
+ASSEM_REF=1
+MAX_ROUNDS=1
+MAX_ITER_ASSEM=1
+INS_T=2
+DEL_T=2
+
+MATCH_PROG="BLAT"
+SORT_PROG="BLAT"
+ALIGN_PROG="BLAT"
 
 # REFERENCE SET
 REF_SET=$(dirname $REF_SET)/$CUSTOM_REF_FILE
