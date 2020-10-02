5492194163834b763a709fdebe58ddbe5947702a
Samuel Shepard <vfn4@cdc.gov>
2020-10-02 14:21:32 -0400

    Added configurations for CoV and updated IRMA with the new options.

---
+++ IRMA_RES/modules/CoV/config/permissive.sh
@@ -0,0 +1,24 @@
+# HEADER
+PARAM_FILE_NAME="Permissive mode"
+PARAM_FILE_AUTHOR="S. Shepard"
+PARAM_FILE_VERSION="1.0"
+PARAM_FILE_DATE="2020-10"
+
+INS_T=0.25		# minimum frquenncy threshold for insertion refinement
+DEL_T=0.75		# minimum frequency threshold for deletion refinement 
+INS_T_DEPTH=1		# minimum coverage depth for insertion refinement
+DEL_T_DEPTH=1		# minimum coverage depth for deletion refinement 
+
+DEL_TYPE="REF NNN"
+ASSEM_REF=1
+ALIGN_PROG="BLAT"
+ASSEM_PROG="SSW"
+MAX_ROUNDS=5
+
+QUAL_THRESHOLD=10			# average or median threshold for QUALITY reads
+MIN_LEN=50				# minimum read length for QUALITY reads
+MIN_RP=1				# minimum read pattern count to continue
+MIN_RC=1				# minimum read count to continue
+
+MIN_CONS_SUPPORT=1	# consensus allele minimum count
+MIN_CONS_QUALITY=1	# consensus allele minimum average quality
