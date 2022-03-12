d246d12f25e2eb9f84655c061691c3577431cfd1
Ben Rambo-Martin <nbx0@cdc.gov>
2022-03-12 09:46:07 -0500

    dangling files needing proper merging

---
+++ IRMA_RES/modules/CoV/config/illumina-2x50.sh
@@ -0,0 +1,8 @@
+# HEADER
+PARAM_FILE_NAME="Illumina 2x100 configuration"
+PARAM_FILE_AUTHOR="B. Rambo-Martin"
+PARAM_FILE_VERSION="1.0"
+PARAM_FILE_DATE="2021-03-23"
+
+MIN_LEN=25      # minimum read length
+
